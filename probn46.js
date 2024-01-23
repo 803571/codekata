8fdabf01512a6ac052e52185134b8d00b65b75a9
803571 <8omsut@naver.com>
2024-01-23 09:15:21 +0900

    46번 문제까지

---
+++ probn46.js
@@ -0,0 +1,42 @@
+// 46번 문제
+// 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+// 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+// 1478 → "one4seveneight"
+// 234567 → "23four5six7"
+// 10203 → "1zerotwozero3"
+// 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+
+// 참고로 각 숫자에 대응되는 영단어는 다음 표와 같습니다.
+
+// 숫자	영단어
+// 0	zero
+// 1	one
+// 2	two
+// 3	three
+// 4	four
+// 5	five
+// 6	six
+// 7	seven
+// 8	eight
+// 9	nine
+
+function solution(s) {
+    s = s.replace(/zero/g, 0)
+    .replace(/one/g, 1)
+    .replace(/two/g, 2)
+    .replace(/three/g, 3)
+    .replace(/four/g, 4)
+    .replace(/five/g, 5)
+    .replace(/six/g, 6)
+    .replace(/seven/g, 7)
+    .replace(/eight/g, 8)
+    .replace(/nine/g, 9)
+    return Number(s);
+}
+
+// 처음에 진짜 모르겠는데 정규 표현식을 쓰면 생각보다 해결이 굉장히 쉬웠다.
+// /g는 전역적으로 이 효과를 발생시킨다는 의미로, .replace(/숫자를영어로, 숫자) 를 하면 최초 1번만 발생하는데, /g를 작성하므로써 모든 발생을 바꾸게 됨
+// 시간날 때 정규표현식에 대해 공부해봐야겠더라
+
